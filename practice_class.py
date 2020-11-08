0a188f54b8f76ba35c6c88770bcfa1fe987b3553
sangHyun <public.winverse@gmail.com>
2020-11-08 23:23:40 +0900

    super

--- practice_class.py
+++ practice_class.py
@@ -67,12 +67,29 @@ class FlyableAttcakUnit(AttackUnit, Flyable):
 # valkyrie.fly(valkyrie.name, "12시")
 
 
-# 벌쳐 : 지상 유닛, 기동성이 좋음
-vulture = AttackUnit("벌쳐", 80, 10, 20)
+# # 벌쳐 : 지상 유닛, 기동성이 좋음
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
+# # 배틀크루저 : 공중 유닛, 체력, 공격력이 좋음
+#
+# battlecruiser = FlyableAttcakUnit("배틀크루저", 500, 25, 3)
+#
+# vulture.move("11시")
+# battlecruiser.move("9시")
 
-# 배틀크루저 : 공중 유닛, 체력, 공격력이 좋음
-battlecruiser = FlyableAttcakUnit("배틀크루저", 500, 25, 3)
+# 건물
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)
+        self.location = location
 
-vulture.move("11시")
-battlecruiser.move("9시")
-
+# # 서플라이디폿 : 건물 1개 = 8 유닛
+# supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+#
+#
+# def game_start():
+#     print("[알림] 새로운 게임을 시작합니다.")
+#
+#
+# def game_over():
+#     pass
