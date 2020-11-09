b1b040d2d2bf69a37ab7d73a5f2661c7abfe6f44
sangHyun <public.winverse@gmail.com>
2020-11-09 19:54:45 +0900

    seize tank

--- practice_class.py
+++ practice_class.py
@@ -3,20 +3,18 @@ class Unit:
         self.name = name
         self.hp = hp
         self.speed = speed
+        print(f"{name} 유닛이 생성되었습니다.")
 
     def move(self, location):
         print("[지상 유닛 이동]")
         print(f"{self.name} : {location} 방향으로 이동합니다. [속도 {self.speed}]")
 
-# marine1 = Unit("마린", 40, 5)
-# tank = Unit("탱크", 150, 35)
-
-# wraith1 = Unit("레이스", 80, 5)
-# wraith2 = Unit("레이스", 80, 5)
-# wraith2.clocking = True
-
-# if wraith2.clocking == True:
-#   print(f"{wraith2.name}는 현재 클로킹 상태 입니다.")
+    def damaged(self, damage):
+        print(f"{self.name} : {damage} 데미지를 입었습니다.")
+        self.hp -= damage
+        print(f"{self.name} : 현재 체력은 {self.hp} 입니다.")
+        if self.hp <= 0:
+            print(f"{self.name} : 파괴되었습니다.")
 
 
 class AttackUnit(Unit):
@@ -34,15 +32,7 @@ class AttackUnit(Unit):
 
         if self.hp <= 0:
             print(f"{self.name} 유닛이 파괴되었습니다.")
-      
 
-# # 파이어벳 : 공격 유닛, 화염방사기
-# firebat1 = AttackUnit("파이어뱃", 50, 16)
-# firebat1.attack("5시")
-#
-# # 공격 2번 받는다고 가정
-# firebat1.damaged(25)
-# firebat1.damaged(25)
 
 class Flyable:
     def __init__(self, flying_speed: int):
@@ -62,19 +52,6 @@ class FlyableAttcakUnit(AttackUnit, Flyable):
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
-# valkyrie = FlyableAttcakUnit('발키리', 200, 6, 5)
-# valkyrie.fly(valkyrie.name, "12시")
-
-
-# # 벌쳐 : 지상 유닛, 기동성이 좋음
-# vulture = AttackUnit("벌쳐", 80, 10, 20)
-# # 배틀크루저 : 공중 유닛, 체력, 공격력이 좋음
-#
-# battlecruiser = FlyableAttcakUnit("배틀크루저", 500, 25, 3)
-#
-# vulture.move("11시")
-# battlecruiser.move("9시")
 
 # 건물
 class BuildingUnit(Unit):
@@ -83,13 +60,83 @@ class BuildingUnit(Unit):
         super().__init__(name, hp, 0)
         self.location = location
 
-# # 서플라이디폿 : 건물 1개 = 8 유닛
-# supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
-#
-#
-# def game_start():
-#     print("[알림] 새로운 게임을 시작합니다.")
-#
-#
-# def game_over():
-#     pass
+
+# 마린
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self,  "마린", 40, 1, 5)
+
+        # 스팀팩 : 일정시간 동안 공격력 속도를 증가, 체력 10 감소
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print(f"{self.name} : 스팀팩을 사용합니다. (HP 10 감소)")
+        else:
+            print(f"{self.name} : 체력이 부족하여 스팀팩을 사용하지 않습니다.")
+
+
+# 탱크
+class Tank(AttackUnit):
+    # 시즈 모드 : 탱크를 지상에 고정시켜, 더 높은 파워로 공격 가능
+    seize_developed = False  # 시즈모드 개발여부
+
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+        self.seize_mode = False
+
+    def set_seize_mode(self):
+        if not Tank.seize_developed:
+            return
+
+        if not self.seize_mode:
+            # 현재 시즈모드가 아닐때, -> 시즈모드
+            print(f"{self.name} : 시즈 모드로 전환합니다.")
+            self.damage *= 2
+            self.seize_mode = True
+        else:
+            # 현재 시즈모드 일때, -> 시즈모드 해제₩
+            print(f"{self.name} : 시즈모드를 해제 합니다.")
+            self.damage /= 2
+            self.seize_mode = False
+
+
+# 레이스
+class Wraith(FlyableAttcakUnit):
+    def __init__(self):
+        FlyableAttcakUnit.__init__(self, "레이스", 80, 20, 5)
+        self.clocked = False  # 클로킹 모드(해제 상태)
+
+    def cloking(self):
+        if self.clocked:
+            print(f"{self.name} : 클로킹 모드 해제합니다.")
+            self.clocked = False
+        else:
+            print(f"{self.name} : 클로킹 모드 설정합니다.")
+            self.clocked = True
+
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+
+def game_over():
+    print("Player : gg")
+    print("[Player] 님이 게임에서 퇴장하셨습니다.")
+
+
+# 실제 게임 진행
+game_start()
+
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+t1 = Tank()
+t2 = Tank()
+
+w1 = Wraith()
+w2 = Wraith()
+
+
+# 유닛 일괄 관리
+attack_units = [m1, m2, m3, t1, t2, w1]
